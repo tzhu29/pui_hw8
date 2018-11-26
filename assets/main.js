a3378d327d0f0cd9853576a81498125e7d192664
Tiffany Zhu <tiffanyzhu29@gmail.com>
2018-11-25 22:55:18 -0500

    adding animations

---
+++ assets/main.js
@@ -0,0 +1,16 @@
+$(window).on("load", function() {
+    var offset = $("#sidebar").offset();
+    var scrollDelay = 0
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > offset.top) {
+            $("#sidebar").stop().animate({
+                marginTop: $(window).scrollTop() - offset.top
+            }, scrollDelay, "linear");
+        } else {
+            $("#sidebar").stop().animate({
+                marginTop: 0
+            }, scrollDelay, "linear");
+        };
+    });
+});
+
